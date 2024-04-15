04ca10e56ee2c142da627103229285887b2e4379
Nav <navv5803@gmail.com>
2024-04-15 12:45:25 +0530

    Feature : error feature

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 app.use(cors(
     {
         credentials: true,
-        origin: 'http://localhost:3000',
+        origin: 'https://sharearxus.vercel.app',
     }
 ))
 
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://sharearxus.vercel.app',
     }
 })
 
